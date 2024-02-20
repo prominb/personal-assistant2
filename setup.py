6ea7d5eed1b86fbfd940e6b1127d979cda17b6e0
Roman M <prominb@gmail.com>
2024-02-20 06:02:30 +0200

    edit setup-file e003

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages  # find_namespace_packages
+from setuptools import setup, find_namespace_packages  # find_packages
 
 setup(
     name='personal-assistant2',
@@ -12,11 +12,12 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"],
-    # packages=find_namespace_packages(),
-    packages=find_packages(where="personal_assistant2"),
-    package_dir={"": "personal_assistant2"},
-    # data_files=[('personal_assistant2', ['personal_assistant2/contacts.json', 'personal_assistant2/notes.json'])],
+    packages=find_namespace_packages(),
+    # packages=find_packages(where="personal_assistant2"),
+    # package_dir={"": "personal_assistant2"},
+    data_files=[('personal_assistant2', ['personal_assistant2/contacts.json', 'personal_assistant2/notes.json'])],
     include_package_data=True,
+    package_data={"personal_assistant2": ["*.json", "*.txt"]},
     install_requires=[
         'prompt-toolkit>=3.0'],
     entry_points={'console_scripts': ['run-assistant = personal_assistant2.main:run_bot']},
