be72162b2deaa5ba817500718012863bb0839e2a
Roman M <prominb@gmail.com>
2024-02-20 04:57:15 +0200

    edit setup-file and init-file

--- personal_assistant2/__init__.py
+++ personal_assistant2/__init__.py
@@ -1,4 +1,4 @@
-from src.personal_assistant.main import run_bot
+from personal_assistant2.main import run_bot
 
 
 __all__ = ['run_bot']
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='personal-assistant',
+    name='personal-assistant2',
     version='0.0.1',
     description='Personal assistant helps you organize your notes and address book.',
-    url='https://github.com/prominb/personal-assistant',
+    url='https://github.com/prominb/personal-assistant2',
     author='Olds coders',
     author_email='flyingcircus@example.com',
     license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
     packages=find_namespace_packages(),
+    data_files=[('personal_assistant2', ['personal_assistant2/contacts.json', 'personal_assistant2/notes.json'])],
+    include_package_data=True,
     install_requires=['markdown'],
     include_package_data=True,
-    entry_points={'console_scripts': ['run-assistant = personal_assistant:run_bot']},
+    entry_points={'console_scripts': ['run-assistant = personal_assistant2.main:run_bot']},
     zip_safe=False
 )
